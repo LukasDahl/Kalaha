9edaa116918a58f6fc2b605d71f351f0e63eb980
LukasDahl <s160068@student.dtu.dk>
2020-03-03 11:05:49 +0100

    Damn

--- src/com/AI/Main.java
+++ src/com/AI/Main.java
@@ -3,15 +3,68 @@ package com.AI;
 public class Main {
 
     public static void main(String[] args) {
+        int[] hjælp = {0,0,0,0,0,0};
+
+        State test = new State(hjælp, hjælp, 2, 3 , 1);
+        printBoard(test);
 
 
+        while(true){
 
 
+        }
 
     }
 
-    public void printBoard(State state){
+    public static void printBoard(State state){
+        System.out.printf(" ");
+        for (int i = 5; i >= 0; i-- ) {
+            System.out.printf("%3d", state.getSide2()[i]);
+        }
+        System.out.printf("\n%2d                 %2d\n ", state.getGoal2(), state.getGoal1());
+        for (int i: state.getSide1()) {
+            System.out.printf("%3d", i);
+        }
 
+    }
+
+    public static State turnOutcome(State state, int cup){
+        int hand;
+        int[] side1 = state.getSide1().clone();
+        int[] side2 = state.getSide2().clone();
+        int goal1 = state.getGoal1(), goal2 = state.getGoal2();
+        int handPosition;
+        if (state.getTurn() == 1){
+            hand = side1[cup];
+            side1[cup] = 0;
+            handPosition = cup + 1;
+        }
+        else {
+            hand = side2[cup];
+            side2[cup] = 0;
+            handPosition = cup + 7 + 1;
+        }
+
+        while(hand > 0){
+            handPosition = handPosition % 14;
+
+
+
+
+            handPosition++;
+            hand--;
+        }
+
+        int newTurn = changeTurn(state.getTurn());
+
+
+
+        return null;
+    }
+
+    public static int changeTurn(int turn){
+        turn = 1 + (1 - (turn - 1));
+        return  turn;
     }
 
 }
--- src/com/AI/State.java
+++ src/com/AI/State.java
@@ -1,5 +1,35 @@
 package com.AI;
 
 public class State {
+    private int[] side1 = new int[6];
+    private int[] side2 = new int[6];
+    private int goal1 = 0, goal2 = 0;
+    private int turn;
 
+    public State(int[] side1, int[] side2, int goal1, int goal2, int turn) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.goal1 = goal1;
+        this.goal2 = goal2;
+    }
+
+    public int[] getSide1() {
+        return side1;
+    }
+
+    public int[] getSide2() {
+        return side2;
+    }
+
+    public int getGoal1() {
+        return goal1;
+    }
+
+    public int getGoal2() {
+        return goal2;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
 }
