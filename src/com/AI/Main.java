927dfd766e2dc1cf4ec4955f326119b2c2c4302b
LukasDahl <s160068@student.dtu.dk>
2020-03-10 11:39:58 +0100

    I mean, it's working

--- src/com/AI/Main.java
+++ src/com/AI/Main.java
@@ -1,28 +1,87 @@
 package com.AI;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    static int DEPTH = 10;
+    static int bestChoice = 0;
+
     public static void main(String[] args) {
         int[] initSide = {6,6,6,6,6,6};
 
-        State initState = new State(initSide, initSide.clone(), 0, 0 , 1);
-        State currentState = initState;
+        State initState = new State(initSide, initSide.clone(), 0, 0 , 1, 0, 0);
+        State currentState = initState, tempState, workingState;
         printBoard(initState);
         Scanner input = new Scanner(System.in);
         int choice;
+        boolean gameover;
+        List<State> frontier = new ArrayList<>();
+        List<Integer> childResults;
+
         while(true){
-            while (true) {
-                choice = input.nextInt();
-                if (choice >= 0 && choice <= 5)
-                    break;
+
+
+            if (currentState.getTurn() == 1) {
+                while (true) {
+                    try {
+                        choice = input.nextInt() - 1;
+                    } catch (InputMismatchException e) {
+                        choice = 6;
+                        input = new Scanner(System.in);
+                    }
+
+                    if (choice >= 0 && choice <= 5)
+                        break;
+                }
+                currentState = turnOutcome(currentState, choice);
             }
-            if (currentState.getTurn() == 2){
-                choice += 7;
+            else{
+                //AI HERE PLS
+                workingState = new State(
+                        currentState.getSide1().clone(),
+                        currentState.getSide2().clone(),
+                        currentState.getGoal1(),
+                        currentState.getGoal2(),
+                        currentState.getTurn(),
+                        currentState.getDepth(),
+                        currentState.getValue()
+                );
+
+                minimax(workingState);
+
+                System.out.printf("Choosing %d as this is the freaking best\n", (bestChoice + 1));
+                currentState = turnOutcome(currentState, bestChoice + 7);
+
+
             }
-            currentState = turnOutcome(currentState, choice);
+
             printBoard(currentState);
+            DEPTH++;
+
+
+            gameover = true;
+            for (int i: currentState.getSide1()){
+                if(i > 0){
+                    gameover = false;
+                }
+            }
+            if (gameover){
+                break;
+            }
+
+            gameover = true;
+            for (int i: currentState.getSide2()){
+                if(i > 0){
+                    gameover = false;
+                }
+            }
+            if (gameover){
+                break;
+            }
 
         }
 
@@ -41,23 +100,23 @@ public class Main {
 
     }
 
-    public static State turnOutcome(State state, int cup){
+    public static State turnOutcome(State state, int pocket){
         int hand;
         int[] side1 = state.getSide1().clone();
         int[] side2 = state.getSide2().clone();
         int goal1 = state.getGoal1(), goal2 = state.getGoal2();
         int handPosition;
       //  System.out.println(state.getTurn() + "");
-        if (cup < 6){
+        if (pocket < 6){
 
-            hand = side1[cup];
-            side1[cup] = 0;
-            handPosition = cup;
+            hand = side1[pocket];
+            side1[pocket] = 0;
+            handPosition = pocket;
         }
         else {
-            hand = side2[cup - 7];
-            side2[cup - 7] = 0;
-            handPosition = cup;
+            hand = side2[pocket - 7];
+            side2[pocket - 7] = 0;
+            handPosition = pocket;
         }
 
         while(hand > 0){
@@ -102,28 +161,39 @@ public class Main {
             hand--;
         }
 
-        //DEFINE A STATE
-        State tempState = new State(side1.clone(), side2.clone(), goal1, goal2, state.getTurn());
+        int nextTurn = state.getTurn();
 
         if (handPosition < 6){
-            if (side1[handPosition] > 1){
-                tempState = turnOutcome(tempState, handPosition);
+            if (side1[handPosition] == 1){
+                if (state.getTurn() == 1){
+                    goal1 += side1[handPosition] + side2[5-handPosition];
+                    side1[handPosition] = 0;
+                    side2[5-handPosition] = 0;
+                }
             }
-            else tempState.setTurn(changeTurn(state.getTurn()));
+            nextTurn = changeTurn(state.getTurn());
         }
         else if (handPosition < 7){
 
         }
         else if (handPosition < 13){
-            if (side2[handPosition-7] > 1){
-                tempState = turnOutcome(tempState, handPosition);
+            if (side2[handPosition-7] == 1){
+                if (state.getTurn() == 2){
+                    goal2 += side2[handPosition-7] + side1[5-(handPosition-7)];
+                    side2[handPosition-7] = 0;
+                    side1[5-(handPosition-7)] = 0;
+                }
             }
-            else tempState.setTurn(changeTurn(state.getTurn()));
+            nextTurn = changeTurn(state.getTurn());
 
         }
         else {
 
         }
+
+        //DEFINE A STATE
+        State tempState = new State(side1.clone(), side2.clone(), goal1, goal2, nextTurn, state.getDepth() + 1, goal2 - goal1);
+
 
         return tempState;
     }
@@ -132,5 +202,59 @@ public class Main {
         turn = 1 + (1 - (turn - 1));
         return  turn;
     }
+
+    public static int minimax(State workingState){
+        State tempState;
+        List<Integer> childResults = new ArrayList<>();
+
+        if (workingState.getDepth() == DEPTH){
+            return workingState.getValue();
+        }
+        for (int pocket = 0; pocket< workingState.getSide2().length; pocket++){
+            if (workingState.getSide2()[pocket] == 0) {
+                childResults.add(null);
+                continue;
+            }
+            if (workingState.getTurn() == 1){
+                tempState = turnOutcome(workingState, pocket);
+                childResults.add(minimax(tempState));
+            }
+            else {
+                tempState = turnOutcome(workingState, pocket + 7);
+                childResults.add(minimax(tempState));
+            }
+            workingState.getChildren().add(tempState);
+        }
+
+
+        for (State state: workingState.getChildren()){
+
+        }
+        int result;
+        if (workingState.getTurn() == 1){
+            result = 72;
+            for (int i = 0; i < childResults.size(); i++){
+                if (childResults.get(i) != null) {
+                    if (childResults.get(i) < result) {
+                        result = childResults.get(i);
+                        bestChoice = i;
+                    }
+                }
+            }
+        }
+        else{
+            result = -72;
+            for (int i = 0; i < childResults.size(); i++){
+                if (childResults.get(i) != null) {
+                    if (childResults.get(i) > result) {
+                        result = childResults.get(i);
+                        bestChoice = i;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 
 }
--- src/com/AI/State.java
+++ src/com/AI/State.java
@@ -1,33 +1,57 @@
 package com.AI;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class State {
+    List<State> children = new ArrayList<>();
     private int[] side1 = new int[6];
     private int[] side2 = new int[6];
     private int goal1 = 0, goal2 = 0;
     private int turn;
+    private int depth;
+    private int value;
 
-    public State(int[] side1, int[] side2, int goal1, int goal2, int turn) {
+    public State(int[] side1, int[] side2, int goal1, int goal2, int turn, int depth, int value) {
         this.side1 = side1;
         this.side2 = side2;
         this.goal1 = goal1;
         this.goal2 = goal2;
         this.turn = turn;
+        this.depth = depth;
+        this.value = value;
     }
 
     public int[] getSide1() {
         return side1;
     }
 
+    public void setSide1(int[] side1) {
+        this.side1 = side1;
+    }
+
     public int[] getSide2() {
         return side2;
+    }
+
+    public void setSide2(int[] side2) {
+        this.side2 = side2;
     }
 
     public int getGoal1() {
         return goal1;
     }
 
+    public void setGoal1(int goal1) {
+        this.goal1 = goal1;
+    }
+
     public int getGoal2() {
         return goal2;
+    }
+
+    public void setGoal2(int goal2) {
+        this.goal2 = goal2;
     }
 
     public int getTurn() {
@@ -36,5 +60,25 @@ public class State {
 
     public void setTurn(int turn) {
         this.turn = turn;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public List<State> getChildren() {
+        return children;
     }
 }
